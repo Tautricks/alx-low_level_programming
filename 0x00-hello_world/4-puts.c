ec92dad59fcac523a9cc20274aabba714d9f0db8
Tautricks <Patrick.Njobvu>
2023-02-24 05:43:32 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-include <stdio>
-
-int main(void):
+#include <stdio.h>
+/**
+ * main - Entry point
+ * first c program
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
