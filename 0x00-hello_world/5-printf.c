77d8bea98aa57d038dc068fd3e0a6b54778ae992
Tautricks <Patrick.Njobvu>
2023-02-24 06:19:16 -0800

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main -  using printf to print on screen
+ * Return: zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
