ba7d5013e8a85b954e1ab210014f8a37d6eb2fa8
Tautricks <Patrick.Njobvu>
2023-02-24 07:50:08 -0800

    9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
+
 #include <stdio.h>
-#include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Description: 'this pring print using printf'
- *
- * Return: 1
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-return (1);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(str, sizeof(str), 1, stderr);
+	return (1);
 }
