87712b072a6c1b652f0681e3caa622a4837ecb5d
Tautricks <Patrick.Njobvu>
2023-02-28 01:49:23 -0800

    9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	scanf(str, sizeof(str), 1, stderr);
 
+	scanf(str, sizeof(str), 1, stderr);
 	return (1);
 }
