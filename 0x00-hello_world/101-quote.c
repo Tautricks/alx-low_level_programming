304417d0d0d9563d6bf35922533559db6a997c14
Tautricks <Patrick.Njobvu>
2023-02-28 01:39:27 -0800

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@ int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fputs(str, sizeof(str), 1, stderr);
+	putchar(str, sizeof(str), 1, stderr);
 	return (1);
 }
